c7d501187d0ed920f0b293192b8f3b5106d58ac2
mh <mhunter@jawbone.com>
2016-05-12 11:12:10 -0700

    Fixing redirect uri

--- example.rb
+++ example.rb
@@ -3,6 +3,7 @@ require 'oauth2'
 
 CLIENT_ID = CLIENT_ID_HERE
 CLIENT_SECRET = CLIENT_SECRET_HERE
+REDIRECT_URI = REDIRECT_URI_HERE
 
 def client
   OAuth2::Client.new(CLIENT_ID, CLIENT_SECRET,
@@ -15,7 +16,7 @@ end
 get '/' do 
   params = { 
     :scope => 'basic_read', 
-    :redirect_uri => 'http://127.0.0.1:9393/auth' 
+    :redirect_uri => REDIRECT_URI
   } 
   redirect client.auth_code.authorize_url(params) 
 end 
